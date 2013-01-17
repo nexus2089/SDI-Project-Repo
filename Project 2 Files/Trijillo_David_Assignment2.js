b363c1ef918d38aecfaca3a8bef1a9b121590de7
David Trijilo <trijillo_david@yahoo.com>
2013-01-17 16:14:37 -0500

    Fresh start

--- Project 2 Files/Trijillo_David_Assignment2.js
+++ Project 2 Files/Trijillo_David_Assignment2.js
@@ -1,8 +1,15 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
 
-var getReady = ["wake up", "take shower", "brush teeth", "get dressed", "go to work"];
-var time = true;
+var startDay = ["wake up", "take shower", "brush teeth", "get dressed", "go to work"];
+var name = ("David");
+var morning = true
+var cold = false
 
 
-var wakeUp = function (0)
+if (morning === true) {
+	console.log(" My alarm wakes me up at 6 am");
+}   else { console.log("It's not time to get up yet so " + name + " goes back to sleep")
+	};
+
+
 
